8144c4130e8dd5278fc3a983fd91e2b892b6f63f
Василий Скребков <svawork@gmail.com>
2020-03-16 16:15:18 +0500

    test commit

---
+++ 1/script.js
@@ -0,0 +1,16 @@
+'use strict';
+
+
+var leftBorderWidth=1;
+let second=2;
+const pi=3.14;
+
+alert ('Hello');
+
+//let answer = confirm('Are you here?');
+
+//console.log(answer);
+
+let answer = prompt('Are you 18?', 'Yes');
+
+console.log(answer);
